0da84d4ac8b484bba3e8ee862be4a5141b47698f
izerque <isaacmimo118@gmail.com>
2023-05-04 13:26:37 +0300

    latest update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Logo from './assets/logo.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full h-screen flex flex-col mt-auto mb-auto justify-center items-center" >
+      <img src={Logo} height='80' width='80' className='img' alt="" />
+      <h1 className='text-gray-600 font-bold text-4xl'>
+        Gift <span className='text-pink-600 font-medium'>Me</span>
+      </h1>
+      <p className='font-light text-lg'>Coming soon </p>
+     
     </div>
   );
 }
